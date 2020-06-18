39ac43b7d7bbd097c0ccecf1a3e4db5aee10ce15
Pavel <zeroblaze@yandex.ru>
2020-06-18 22:31:02 +0300

    Добавлено окно редактирования и базовая привязка на главном окне

---
+++ CompanyEmployee/EmployeeEditWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using CompanyEmployee.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CompanyEmployee
+{
+    /// <summary>
+    /// Логика взаимодействия для EmployeeEditWindow.xaml
+    /// </summary>
+    public partial class EmployeeEditWindow : Window
+    {
+        public Employee editableObject;
+        public EmployeeEditWindow()
+        {
+            InitializeComponent();
+        }
+        public EmployeeEditWindow(MainWindow parentWindow): this()
+        {
+            Employee employee = (parentWindow.lvEmployee.SelectedItem as Employee);
+            editableObject = employee.Clone() as Employee;
+        }
+        public EmployeeEditWindow(Employee employeeToEdit) : this()
+        {
+            Employee employee = (parentWindow.lvEmployee.SelectedItem as Employee);
+            editableObject = employee.Clone() as Employee;
+        }
+
+    }
+}
--- CompanyEmployee/Model/Employee.cs
+++ CompanyEmployee/Model/Employee.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CompanyEmployee.Model
 {
-    public class Employee
+    public class Employee : ICloneable
     {
         private static int _id;
-        public int Id { get; }
+        public int Id { get; private set; }
         public string Name { get; set; }
         public string Surname { get; set; }
         public int Age { get; set; }
@@ -20,18 +21,30 @@ namespace CompanyEmployee.Model
             set => _department.Name = value; 
         }
         Department _department;
-    public Employee(string name, string surname, int age, int salary, string department)
+
+        public Employee() 
         {
-            Id = ++_id;
+        }
+        public Employee(string name, string surname, int age, double salary, string department)
+        {
+            Id = _id++;
             Name = name;
             Surname = surname;
             Age = age;
             Salary = salary;
             _department = new Department(department);
         }
-        public override string ToString()
+        public object Clone()
         {
-            return $"{Id,3}\t{Name,15}\t{Surname,15}\t{Age,4}\t{Salary,10}\t{_department}";
+            return new Employee()
+            {
+                Id = this.Id,
+                Name = this.Name,
+                Surname = this.Surname,
+                Age = this.Age,
+                Salary = this.Salary,
+                Department = this.Department
+            };
         }
     }
 }
