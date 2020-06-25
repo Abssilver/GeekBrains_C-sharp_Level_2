bf22f853c209517419def1c7ab41e9193f380a1c
Pavel <zeroblaze@yandex.ru>
2020-06-25 19:09:15 +0300

    preporations for homework 8

---
+++ WebAPIService/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIService.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; }
+        public string Department { get; set; }
+        
+    }
+}
